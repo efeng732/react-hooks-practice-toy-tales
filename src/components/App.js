ede45551eda553d4d70467e14b9a279bd1b20373
Erwin <efeng732@gmail.com>
2021-02-09 08:57:10 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,44 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+    .then(res => res.json())
+    .then(toysArray => setToys(toysArray))
+  })
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addToy(newToy) {
+    const updatedArr = [...toys, newToy]
+    setToys(updatedArr)
+  }
+
+  function deleteToy(deleteToy){
+    const updatedToys = toys.filter((toy) => toy.id !== deleteToy.id
+
+    )
+    setToys(updatedToys)
+
+  }
+
+
+  function updateToy(t) {
+    const updatedToys = toys.map((toy) => toy.id === t.id? t : toy)
+    setToys(updatedToys)
+  }
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer deleteToy={deleteToy} updateToy={updateToy} toys={toys}/>
     </>
   );
 }
--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({toy, deleteToy, updateToy}) {
+
+  function handleDelete() {
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(toy => deleteToy(toy))
+
+  }
+
+  function handleLike(){
+    fetch(`http://localhost:3001/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json"
+      }, 
+      body: JSON.stringify({
+        ...toy, likes: parseInt(toy.likes + 1)
+      })
+    })
+    .then(res => res.json())
+    .then(t => updateToy (t))
+
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button onClick={handleLike} className="like-btn">Like {"<3"}</button>
+      <button onClick={handleDelete} className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
